10ac8c183f7e24cd1ef56898acb9856e04a33d41
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-09-12 23:55:32 +0200

    [UPDATE] Make the exercise 1.d

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ let allData;
 
 fetch('https://gist.githubusercontent.com/Oskar-Dam/62e7175dc542af53a9d18cb292422425/raw/a6cce2b68ea13a77ec5ea7bdfb4df8f23f9ae95f/donuts.json')
     .then(response => response.json())
-    .then(data => { 
-        
-        allData = data; 
-    
+    .then(data => {
+
+        allData = data;
+
         console.log("\n---------------------------------------------------------------------------------\n");
-        
-    
+
+
         exercise1(allData);
 
 
-    
+
     });
 
 
@@ -23,15 +23,16 @@ fetch('https://gist.githubusercontent.com/Oskar-Dam/62e7175dc542af53a9d18cb29242
 function exercise1(allData) {
 
     console.log("Exercise 1\n");
-    
 
     exercise1Sugar(allData);
-    console.log("");(allData);
+    console.log(""); (allData);
     exercise1Iron(allData);
     console.log("");
     exercise1Protein(allData)
     console.log("");
-    
+    exercise1Fiber(allData);
+    console.log("");
+
 }
 
 function exercise1Sugar(allData) {
@@ -40,37 +41,37 @@ function exercise1Sugar(allData) {
     let sugarQuantity = 0;
     let names = [];
     // console.log(allData..item[0].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars);
-    
+
     for (let i = 0; i < allData.items.item.length; i++) {
         const item = allData.items.item[i];
 
         let itemSugar = item.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars;
         itemSugar = itemSugar.replace("g", "");
         itemSugar = parseInt(itemSugar);
-        
+
         if (itemSugar === sugarQuantity) {
-            
+
             names.push(item.name);
 
-        }else if( itemSugar > sugarQuantity){
+        } else if (itemSugar > sugarQuantity) {
 
             sugarQuantity = itemSugar;
             names = [];
             names.push(item.name);
-            
+
         }
-        
+
     }
 
 
-    console.log("+ The donut with the most sugar has "+ sugarQuantity +"g: ");
+    console.log("+ The donut with the most sugar has " + sugarQuantity + "g: ");
 
     for (let i = 0; i < names.length; i++) {
 
-        console.log("     ~~~> "+ names[i]);
-        
+        console.log("     ~~~> " + names[i]);
+
     }
-    
+
 }
 
 function exercise1Protein(allData) {
@@ -80,39 +81,38 @@ function exercise1Protein(allData) {
     let names = [];
 
     // console.log(allData.items.item[0].nutrition_facts.nutrition.vitamines[0]);
-    
+
     for (let i = 0; i < allData.items.item.length; i++) {
         const item = allData.items.item[i];
-    
-            let itemProtein = item.nutrition_facts.nutrition.proteine;
-            itemProtein = itemProtein.replace("g", "");
-            itemProtein = parseInt(itemProtein);
 
-            if (itemProtein === proteinQuantity) {
-            
-                names.push(item.name);
-    
-            }else if( itemProtein > proteinQuantity){
-    
-                proteinQuantity = itemProtein;
-                names = [];
-                names.push(item.name);
-                
-            }
-        
+        let itemProtein = item.nutrition_facts.nutrition.proteine;
+        itemProtein = itemProtein.replace("g", "");
+        itemProtein = parseInt(itemProtein);
+
+        if (itemProtein === proteinQuantity) {
+
+            names.push(item.name);
+
+        } else if (itemProtein > proteinQuantity) {
+
+            proteinQuantity = itemProtein;
+            names = [];
+            names.push(item.name);
+
+        }
+
     }
 
 
-    console.log("+ The donut with the most protein has "+ proteinQuantity +"g: ");
+    console.log("+ The donut with the most protein has " + proteinQuantity + "g: ");
 
     for (let i = 0; i < names.length; i++) {
 
-        console.log("     ~~~> "+ names[i]);
-        
-    }
-    
-}
+        console.log("     ~~~> " + names[i]);
 
+    }
+
+}
 
 
 function exercise1Iron(allData) {
@@ -122,7 +122,7 @@ function exercise1Iron(allData) {
     let names = [];
 
     // console.log(allData.items.item[0].nutrition_facts.nutrition.vitamines[0]);
-    
+
     for (let i = 0; i < allData.items.item.length; i++) {
         const item = allData.items.item[i];
         const itemName = item.name;
@@ -136,33 +136,88 @@ function exercise1Iron(allData) {
             itemIron = parseInt(itemIron);
 
             if (itemIron === ironQuantity) {
-            
+
                 names.push(item.name);
-    
-            }else if( itemIron > ironQuantity){
-    
+
+            } else if (itemIron > ironQuantity) {
+
                 ironQuantity = itemIron;
                 names = [];
                 names.push(item.name);
-                
+
             }
         }
 
-    
 
-        
-        
     }
 
 
-    console.log("+ The donut with the most iron has "+ ironQuantity +"%: ");
+    console.log("+ The donut with the most iron has " + ironQuantity + "%: ");
 
     for (let i = 0; i < names.length; i++) {
 
-        console.log("     ~~~> "+ names[i]);
-        
+        console.log("     ~~~> " + names[i]);
+
     }
-    
+
+}
+
+function exercise1Fiber(allData) {
+
+
+    let fiberQuantity = 0;
+    let names = [];
+
+    // console.log(allData.items.item[0].nutrition_facts.nutrition.vitamines[0]);
+
+
+    for (let i = 0; i < allData.items.item.length; i++) {
+        const item = allData.items.item[i];
+
+        let itemFibre = item.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre;
+        itemFibre = itemFibre.replace("g", "");
+        itemFibre = parseInt(itemFibre);
+
+        if (itemFibre === fiberQuantity) {
+
+            names.push(item.name);
+
+        } else if (itemFibre > fiberQuantity) {
+
+            fiberQuantity = itemFibre;
+            names = [];
+            names.push(item.name);
+
+        }
+
+
+
+
+    }
+
+
+    console.log("+ The donut with the most fibre has " + fiberQuantity + "g: ");
+
+    for (let i = 0; i < names.length; i++) {
+
+        console.log("     ~~~> " + names[i]);
+
+    }
+
 }
 
 
+function exercise2(allData) {
+
+    console.log("Exercise 1\n");
+
+    
+    console.log(""); 
+    
+    console.log("");
+    
+    console.log("");
+    
+    console.log("");
+
+}
